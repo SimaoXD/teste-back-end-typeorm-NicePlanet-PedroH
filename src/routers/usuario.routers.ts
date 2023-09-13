083c16688c30154f3606ea04cb36f3600afcdcd6
SimaoXD <simas_2007@hotmail.com>
2023-09-13 09:42:26 -0300

    import schemas in router

--- src/routers/usuario.routers.ts
+++ src/routers/usuario.routers.ts
@@ -5,16 +5,18 @@ import { createUser, getUserList } from "../controllers/usuario.controllers";
 import { verifyTokenUser, verifyUserAdmin } from "../middlewares/validation.middlewares";
 import { createProperty, getPropertyList } from "../controllers/propriedade.controllers";
 import { createProducer, getProducerList } from "../controllers/produtor.controllers";
+import { producerDataRegisterSchema } from "../schemas/produtor.schema";
+import { propertyDataRegisterSchema } from "../schemas/propriedade.schema";
 
 const router = Router();
 
 router.post("/usuario", verifyDataBody(userDataRegisterSchema), createUser);
 router.get("/usuario", verifyTokenUser, verifyUserAdmin, getUserList);
 
-router.post("/produtor", createProducer);
+router.post("/produtor", verifyDataBody(producerDataRegisterSchema), createProducer);
 router.get("/produtor/:id", verifyProducerExists, getProducerList);
 
-router.post("/propriedade", createProperty);
-router.get("/propriedade", getPropertyList);
+router.post("/propriedade", verifyDataBody(propertyDataRegisterSchema), createProperty);
+router.get("/propriedade/:id", getPropertyList);
 
 export default router;
