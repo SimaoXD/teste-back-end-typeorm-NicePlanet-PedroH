15a3d855d88e370d24107381c1bdc06c14e8f5a1
SimaoXD <simas_2007@hotmail.com>
2023-09-13 09:37:23 -0300

    feat: Project Finalized

--- src/routers/usuario.routers.ts
+++ src/routers/usuario.routers.ts
@@ -11,11 +11,9 @@ const router = Router();
 router.post("/usuario", verifyDataBody(userDataRegisterSchema), createUser);
 router.get("/usuario", verifyTokenUser, verifyUserAdmin, getUserList);
 
-// Rotas para Produtores (você pode precisar de middlewares adicionais ou esquemas aqui)
 router.post("/produtor", createProducer);
 router.get("/produtor/:id", verifyProducerExists, getProducerList);
 
-// Rotas para Propriedades (você pode precisar de middlewares adicionais ou esquemas aqui)
 router.post("/propriedade", createProperty);
 router.get("/propriedade", getPropertyList);
 
